f7a832d9c73a8bb76ca5db556fe5aec334a13c74
Ajmal777 <81432526+Ajmal777@users.noreply.github.com>
2023-06-22 18:22:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+const output = document.getElementById('output');
+const res = new Promise((res, rej)=>{
+	setTimeout(()=>{
+		res('Hello, World!');
+	}, 1000);
+});
+res.then((data) => {
+	output.innerText = data;
+});
